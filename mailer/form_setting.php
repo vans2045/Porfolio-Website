212af943b90dfd4ccc44487b5e565f702bb0a66a
vans2045 <105538916+vans2045@users.noreply.github.com>
2025-02-12 22:18:06 +0530

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your portfolio website"; //letter subject
+	$to = 'tanishqchandravanshi@gmail.com'; // Enter Your E-mail
+	$from = 'tanishqchandravanshi@gmail.com'; // Admin e-mail
+	$fromName = 'Tanishq Chandravanshi Porfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
